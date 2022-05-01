334c059fd283aff829f979bad2f47dd383b5224e
Labrosimova <104066968+Labrosimova@users.noreply.github.com>
2022-05-01 17:44:25 +0300

    Add files via upload

---
+++ example2.js
@@ -0,0 +1,51 @@
+
+// const check_age = function (age) {
+//     age = Number(age);
+//     if (!isNaN(age)) {        
+//         if (age > 18) {
+//             alert(`What is your name?`)
+//         // const ask_name = function (username);
+//     } }
+    
+//     if (age > 25){
+//         alert('Welcome 25!')
+//     }
+    
+//     else {
+//         alert('Technical work')
+//     }   
+
+
+//     else {
+//         alert('Error!')
+//     }
+      
+// }
+// let agePrompt = prompt('Enter your age:');
+// // let usernamePrompt = prompt(`What is your name?`)
+// check_age(agePrompt);
+// // ask_name(usernamePrompt);
+
+
+// let age = prompt (How old are you?)
+// let check_age = null;
+// if (age >18){
+//     check_age = 'You';
+//     alert(check_age);
+// }
+// else {
+//     check_age = `Not you`;
+//     alert(check_age);
+// };
+let age = prompt('How old are you?');
+
+if (age > 18) {
+   let username = prompt (`What's your name?`);
+} else {
+    alert `Go away`
+};
+if (username == `John`) {
+    alert(`Welcome, John!`);
+} else {
+    alert(`Who are you? I don't know you.`);
+};
