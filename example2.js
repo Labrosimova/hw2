8664cb825ed1802ce14f2f741645e6d8c7ffecc2
Labrosimova <104066968+Labrosimova@users.noreply.github.com>
2022-05-01 18:32:05 +0300

    Update example2.js

--- example2.js
+++ example2.js
@@ -1,13 +1,12 @@
-
 let age = prompt('How old are you?');
 let username;
 if (age > 18) {
    username = prompt (`What's your name?`);
-} else {
-    alert `Go away`
-};
-if ((age > 25) && (username == `John`)) {
+   if ((age > 25) && (username == `John`)) {
     alert(`Welcome, John!`);
 } else {
     alert(`Who are you? I don't know you.`);
+};
+} else {
+    alert `Go away`
 };
